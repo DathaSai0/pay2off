6ae36994aec8553fb4534636ab4772168364f7a0
Sai Ganesh <saiganesh6841@gmail.com>
2025-04-10 23:03:45 +0530

    Hero section is done

--- src/pages/HeroSection/Header.jsx
+++ src/pages/HeroSection/Header.jsx
@@ -3,6 +3,8 @@ import Logo from "../../assets/Images/Pay2off Logo 1.png";
 import { handleLocateMe } from "../../utilis/location";
 import "./styles/Header.scss";
 import DialogModal from "../../components/DialogModal/Index";
+import searchIcon from "../../assets/Images/searchIcon.png";
+import userFrame from "../../assets/Images/userFrame.png";
 
 const NavBar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -11,28 +13,46 @@ const NavBar = () => {
     setIsModalOpen(true);
   };
   return (
-    <div>
-      <nav className="header">
-        <div className="logo">
-          <img src={Logo} />
-          <span>Pay2off</span>
-        </div>
-        <div className="location-section">
-          <div className="location_desk" onClick={openLocationModal}>
-            {/* <LocationIcon /> */}
+    <>
+      <div className="home_banner">
+        <nav className="header">
+          <div className="content-row">
+            <div className="logo">
+              <img src={Logo} />
+              <span>Pay2off</span>
+            </div>
+
+            <div className="location-wrapper">
+              <div className="location_desk" onClick={openLocationModal}>
+                Coimbatore, Tamil Nadu 641105, India
+              </div>
+
+              <button className="locate-button" onClick={handleLocateMe}>
+                Locate Me
+              </button>
+            </div>
+          </div>
+
+          <div className="location_mobile" onClick={openLocationModal}>
             Coimbatore, Tamil Nadu 641105, India
           </div>
-          <button className="locate-button" onClick={handleLocateMe}>
-            {" "}
-            Locate Me
-          </button>
-        </div>
-      </nav>
-      <div className="location_mobile" onClick={openLocationModal}>
-        {/* <LocationIcon /> */}
-        Coimbatore, Tamil Nadu 641105, India
-      </div>
+        </nav>
 
+        <section>
+          <div className="header_content">
+            <h2>
+              Get<span className="header_primary"> Discounts & Offers</span> on
+              Everything
+            </h2>
+            <div>
+              <img src={searchIcon} />
+            </div>
+          </div>
+          <div className="dynamic_banner">
+            <img src={userFrame} />
+          </div>
+        </section>
+      </div>
       <DialogModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <div className="location-modal">
           <div className="location-title">Setup your Location</div>
@@ -54,7 +74,7 @@ const NavBar = () => {
           </div>
         </div>
       </DialogModal>
-    </div>
+    </>
   );
 };
 export default NavBar;
